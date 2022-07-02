d2fd26123ee5c04032f478da80e34b62286ac075
sardoraminov <oddiydasturchi@gmail.com>
2022-07-02 15:14:35 +0500

    changes

--- app.js
+++ app.js
@@ -5,11 +5,16 @@ const usersRouter = require("./routes/user.routes.js")
 
 const app = express()
 
+
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 app.use(usersRouter)
+
+app.get("/", (req, res) => {
+  res.send("Hello")
+})
 
 app.listen(8080, () => {
   console.log('server is running on 8080 port');
